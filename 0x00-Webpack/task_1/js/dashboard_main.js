e8e7e2e40e97e08474428d42361787bd888faa03
winniewandia <winniewandia63@gmail.com>
2024-05-20 20:31:48 +0300

    added node_modules to gitignore

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,14 +4,14 @@ import _ from 'lodash';
 $.on('load', function() {
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="start-button">Click here to get started</button>');
+$('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
 function updateCounter() {
     count++;
-    $('#count').text(`${count} clicks on the button`);
+    $('#count').html(`${count} clicks on the button`);
 }
 
-$('#start-button').on('click', _.debounce(updateCounter, 300));});
+$('button').on('click', _.debounce(updateCounter, 300));});
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './js/dashboard_main.js',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
