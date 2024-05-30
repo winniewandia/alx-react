07f44db3b9f2df8928ad086cc74d87635c9bac24
winniewandia <winniewandia63@gmail.com>
2024-05-30 12:12:30 +0300

    react components as classes

---
+++ 0x03-React_component/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,25 @@
+import CourseListRow from "./CourseListRow";
+import { shallow } from "enzyme";
+import React from "react";
+
+test("if header is true and textSecondCell is null, CourseListRow renders a th with colSpan 2", () => {
+  const wrapper = shallow(
+    <CourseListRow isHeader={true} textFirstCell="test" textSecondCell={null} />
+    );
+    expect(wrapper.find("th")).toHaveLength(1);
+    expect(wrapper.find("th").prop("colSpan")).toEqual("2");
+});
+
+test("if header is true and textSecondCell is not null, CourseListRow renders two th elements", () => {
+  const wrapper = shallow(
+    <CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test" />
+    );
+    expect(wrapper.find("th")).toHaveLength(2);
+});
+
+test("if header is false, CourseListRow renders two td elements", () => {
+  const wrapper = shallow(
+    <CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test" />
+    );
+    expect(wrapper.find("td")).toHaveLength(2);
+});
---
+++ 0x03-React_component/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './Notifications.css';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+
+function Notification({displayDrawer, listNotifications}) {
+    if (listNotifications.length === 0 && displayDrawer === false) {
+        return (
+            <div className='menuItem'>
+                <p>Your notifications</p>
+                <p>No new notification for now</p>
+            </div>
+        );
+    } else if (listNotifications.length !== 0 && displayDrawer === false) {
+        return (
+            <div className='menuItem'>
+                <p>Your notifications</p>
+                <div className='Notifications'>
+                    <div className='buttonP'>
+                        <p>Here is the list of notifications</p>
+                        <button
+                            style={{border: 'none', background: 'transparent'}}
+                            aria-label='Close'
+                            onClick={() => console.log('Close button has been clicked')}>
+                            <img src= {closeIcon} alt='Close icon' className='closeImage' />
+                        </button>
+                    </div>
+                    <ul className='unorderedList'>
+                        {listNotifications.map((notification) => (
+                            <NotificationItem key={notification.id} type={notification.type} value={notification.value} html={notification.html} />
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+    // return (
+    //     <>
+    //     <div className='menuItem'>
+    //         <p>Your notifications</p>
+    //     </div>
+    //     {displayDrawer && (
+    //     <div className='Notifications'>
+    //         <p>Here is the list of notifications</p>
+    //         {listNotifications && (
+    //         <p>No new notification for now</p>)}
+    //         <ul className='unorderedList'>
+    //             {listNotifications.map((notification) => (
+    //                 <NotificationItem key={notification.id} type={notification.type} value={notification.value} html={notification.html} />
+    //             ))}
+    //         </ul>
+    //         <button
+    //             style={{position: 'absolute', right: '0', border: 'none', background: 'transparent'}}
+    //             aria-label='Close'
+    //             onClick={() => console.log('Close button has been clicked')}>
+    //             <img src= {closeIcon} alt='Close icon' className='closeImage' />
+    //         </button>
+    //     </div>)}
+    //     </>
+    // );
+}
+
+Notification.propTypes = {
+    displayDrawer: PropTypes.bool,
+    listNotifications: PropTypes.array
+};
+
+Notification.defaultProps = {
+    displayDrawer: false,
+    listNotifications: []
+};
+
+export default Notification;
