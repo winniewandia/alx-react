e34cdd2b77702537da47ff9a63f21101d13f0c25
winniewandia <winniewandia63@gmail.com>
2024-06-12 17:37:50 +0300

    merge map, concat list

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,7 +1,7 @@
 const { List, Map } = require('immutable')
 export function concatElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
+  const map1 = List(page1);
+  const map2 = List(page2);
   const map3 = map1.concat(map2);
   return map3;
   // return map1.concat(map2).toJS();
@@ -9,8 +9,8 @@ export function concatElements(page1, page2) {
 }
 
 export function mergeElementsToList(page1, page2) {
-  const list1 = List(page1);
-  const list2 = List(page2);
+  const list1 = Map(page1);
+  const list2 = Map(page2);
   const list3 = list1.merge(list2);
   return list3;
 }
