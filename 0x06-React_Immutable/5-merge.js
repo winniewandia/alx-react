779aab9684ed52c2bd16636ca2b7f6109d1d1812
winniewandia <winniewandia63@gmail.com>
2024-06-12 17:25:24 +0300

    updated 5-merge

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -3,7 +3,7 @@ export function concatElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
   const map3 = map1.concat(map2);
-  return map3.toJS();
+  return map3;
   // return map1.concat(map2).toJS();
   // return page1.concat(page2);
 }
@@ -12,5 +12,5 @@ export function mergeElementsToList(page1, page2) {
   const list1 = List(page1);
   const list2 = List(page2);
   const list3 = list1.merge(list2);
-  return list3.toJS();
+  return list3;
 }
