0aa6b141ff13d88170757c4ee571543c8ec7abc0
winniewandia <winniewandia63@gmail.com>
2024-06-13 17:06:33 +0300

    used require instead of import

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,17 +1,14 @@
-const { List, Map } = require('immutable')
+const { List, Map } = require('immutable');
+
 export function concatElements(page1, page2) {
   const list1 = List(page1);
   const list2 = List(page2);
   return list1.concat(list2);
-  // return page1.concat(page2);
 }
 
 export function mergeElementsToList(page1, page2) {
-  const list1 = Map(page1);
-  const list2 = Map(page2);
-  const list3 = list1.merge(list2);
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+  const list3 = map1.merge(map2);
   return list3;
 }
-
-// console.log(mergeElementsToList({ 1: 'Liam', 2: 'Noah' }, { 3: 'Elijah', 4: 'Oliver' }));
-// console.log(concatElements([1, 2, 3], [4, 5, 6]));
