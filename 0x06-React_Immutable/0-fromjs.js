2043f7392bca41b7679857c391dff660c3fe8db8
winniewandia <winniewandia63@gmail.com>
2024-06-13 12:15:05 +0300

    CommonJS module.exports syntax

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,4 +1,7 @@
 const { fromJS } = require('immutable');
-export function getImmutableObject(object) {
+
+function getImmutableObject(object) {
   return fromJS(object);
 }
+
+module.exports = { getImmutableObject} ;
