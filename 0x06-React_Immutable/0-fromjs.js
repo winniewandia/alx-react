6282c04e7c97cef8e2a87e0e6ab45b8ee029bbb0
winniewandia <winniewandia63@gmail.com>
2024-06-13 12:19:13 +0300

    export default 0-fromjs

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,5 @@
 const { fromJS } = require('immutable');
 
-function getImmutableObject(object) {
+export default function getImmutableObject(object) {
   return fromJS(object);
 }
-
-module.exports = { getImmutableObject} ;
