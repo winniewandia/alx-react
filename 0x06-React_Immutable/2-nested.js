95b953487ea0802b5fe88a24e005f807e2b7115a
winniewandia <winniewandia63@gmail.com>
2024-06-13 17:21:03 +0300

    undefined in nested

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -2,6 +2,6 @@ const { fromJS } = require('immutable');
 
 export default function accessImmutableObject(object, array) {
   const nested = fromJS(object);
-  const value = nested.getIn(array);
-  return value;
+
+  return nested.getIn(array, undefined);
 }
