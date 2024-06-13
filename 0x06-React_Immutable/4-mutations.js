1a6ffcf901ee0a41cf3f5d6c67d9206f8feff21f
winniewandia <winniewandia63@gmail.com>
2024-06-13 17:00:14 +0300

    map.withMutations

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,12 +1,14 @@
-const { fromJS } = require('immutable');
+import { Map } from 'immutable';
 
-export const map = fromJS({
+export const map = Map({
     1: 'Liam',
     2: 'Noah',
     3: 'Elijah',
     4: 'Oliver',
     5: 'Jacob',
-    6: 'Lucas',
+    6: 'Lucas'
 });
 
-export const map2 = map.set('2', Benjamin).set('4', Oliver);
+export const map2 = map.withMutations((mapItem) => {
+    mapItem.set(2, 'Benjamin').set(4, 'Oliver');
+});
